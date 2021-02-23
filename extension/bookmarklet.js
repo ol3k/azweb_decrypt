3fd6c2b68f1087c97be56a584395702cad9881fd
Lasse Moench <lasse.moench@rwth-aachen.de>
2021-02-23 21:07:26 +0100

    Change url parameters to append

--- extension/bookmarklet.js
+++ extension/bookmarklet.js
@@ -22,13 +22,13 @@ for (var i = 0; i < website_links.length; i++) {
         var link = website_links[i].getAttribute("href").split("?")[0];
 
         if (link.match(pattern)) { 
-                website_links[i].setAttribute("href", link + "?utm_medium=instant-message");
-                new_link = link + "?utm_medium=instant-message";                
+                website_links[i].setAttribute("href", link + "?utm_campaign=share&utm_medium=instant-message&utm_medium=referral&utm_source=mail");
+                new_link = link + "?utm_campaign=share&utm_medium=instant-message&utm_medium=referral&utm_source=mail";                
         } else if (link.match(pattern_login) && !window.location.href.includes("instant-message")) {
                 document.getElementsByClassName("park-article__headline")[0].innerHTML = "Click on Image to decrypt!";
                 document.getElementsByClassName("park-article__headline")[0].style.backgroundColor = "red";
                 document.getElementsByClassName("park-article__headline")[0].style.color = "white";
-                website_links[i].setAttribute("href", window.location.href.split("?")[0] + "?utm_medium=instant-message");
+                website_links[i].setAttribute("href", window.location.href.split("?")[0] + "?utm_campaign=share&utm_medium=instant-message&utm_medium=referral&utm_source=mail");
                 website_links[i].removeAttribute("target");             
         }
 }
